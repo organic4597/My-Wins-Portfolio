8480635ef49cb45906f0f7593c73ca6a36b251f7
Pyoint <71939575+organic4597@users.noreply.github.com>
2025-07-24 07:35:12 -0400

    3way-handshake 감지 & 감지된 tcp 아이피 모두 출력 다음 목표: 아이피별로 3way-handshake 정렬 및 추적

--- Tcp-analyzer/capture.c
+++ Tcp-analyzer/capture.c
@@ -2,17 +2,80 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+#include <netinet/tcp.h>
+#include <netinet/ether.h>
+#include <netinet/ip.h>
 typedef unsigned char u_char;
+
+typedef struct tcp_session {
+    struct in_addr src_ip; //출발지 IP 
+    struct in_addr dst_ip; //도착지 IP
+    uint16_t src_port; //출발지 port(16bit)
+    uint16_t dst_port; //도착지 port(16bit)
+
+    struct timeval syn_time;
+    struct timeval ack_time;
+    double rtt;
+
+    size_t bytes_sent;
+    size_t bytes_received;
+
+    uint32_t last_seq;
+    uint32_t retrans_count;
+
+    struct tcp_session *next;
+} tcp_session_t;
+
+//함수 선언
+void packet_cap(const struct pcap_pkthdr *pkthdr, const u_char *packet);
+
+
 
 void packet_handler(u_char *user_data, const struct pcap_pkthdr *pkthdr, const u_char *packet)
 {
-    static int index = 0;
-    printf("packet #%d\n", ++index );
-    printf("Timestamp: %s", ctime((const time_t*)&pkthdr->ts.tv_sec));
-    printf("Packet Length: %d bytes\n",pkthdr->len);
-
+    packet_cap(pkthdr, packet);
 }
+
+
+void packet_cap(const struct pcap_pkthdr *pkthdr, const u_char *packet)
+{
+    struct ip *ip_hdr;
+    struct tcphdr *tcp_hdr;
+
+    // Ethernet 헤더는 14바이트
+    ip_hdr = (struct ip*)(packet + 14);
+
+    // IP가 TCP인지 확인
+    if (ip_hdr->ip_p != IPPROTO_TCP) return;
+
+    int ip_hdr_len = ip_hdr->ip_hl * 4;
+    tcp_hdr = (struct tcphdr*)((u_char*)ip_hdr + ip_hdr_len);
+
+    uint16_t src_port = ntohs(tcp_hdr->th_sport);
+    uint16_t dst_port = ntohs(tcp_hdr->th_dport);
+
+    // TCP flag
+    uint8_t flags = tcp_hdr->th_flags;
+    if ((flags & TH_SYN) && !(flags & TH_ACK)) 
+    {
+        printf("SYN from %s:%d to %s:%d\n",
+            inet_ntoa(ip_hdr->ip_src), src_port,
+            inet_ntoa(ip_hdr->ip_dst), dst_port);
+    }
+    else if ((flags & TH_SYN) && (flags & TH_ACK)) 
+    {
+        printf("SYN-ACK from %s:%d to %s:%d\n",
+            inet_ntoa(ip_hdr->ip_src), src_port,
+            inet_ntoa(ip_hdr->ip_dst), dst_port);
+    }
+    else if ((flags & TH_ACK)) 
+    {
+        printf("ACK from %s:%d to %s:%d\n",
+            inet_ntoa(ip_hdr->ip_src), src_port,
+            inet_ntoa(ip_hdr->ip_dst), dst_port);
+    }
+}
+
 
 int main()
 {
@@ -23,8 +86,8 @@ int main()
     handle = pcap_open_offline(filepath, errbuf);
     if (handle == NULL)
     {
-        printf("pcap_open_offline() failed: %s\n", errbuf);
-    }
+        printf("pcap_open_offline() failed: %c\n", errbuf);
+    } 
 
     pcap_loop(handle, 0, packet_handler, NULL);
 
