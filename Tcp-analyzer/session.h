4d6e72db63077a119568f8f7692cc74d139cbc8c
Pyoint <71939575+organic4597@users.noreply.github.com>
2025-07-26 06:57:55 -0400

    세션 식별 기능 함수 구현 및 각 세션별 패킷수와 바이트 수 그리고고 걸린 왕복 시간 구현

--- Tcp-analyzer/session.h
+++ Tcp-analyzer/session.h
@@ -1,40 +1,28 @@
 #ifndef SESSION_H
 #define SESSION_H
 
-#include <pcap.h>
 #include <netinet/ip.h>
 #include <netinet/tcp.h>
+#include <pcap.h>
 #include <time.h>
 
-#define HASH_TABLE_SIZE 4096
+#define MAX_SESSIONS 1024
 
-typedef struct session_key {
-    struct in_addr src_ip;
-    struct in_addr dst_ip;
-    uint16_t src_port;
-    uint16_t dst_port;
-} session_key_t;
+typedef struct session {
+    char key[128];  // "src_ip:src_port->dst_ip:dst_port"
+    struct timeval start_time;
+    struct timeval last_time;
+    uint32_t packet_count;
+    uint32_t byte_count;
+    struct session *next;
+} session_t;
 
-typedef struct session_data {
-    session_key_t key;
-    uint32_t last_seq;
-    int retransmissions;
-    time_t start_time;
-    time_t end_time;
-    int packet_count;
-    struct session_data* next;  // 해시 충돌 해결용 체이닝
-} session_data_t;
+typedef struct {
+    session_t *buckets[MAX_SESSIONS];
+} session_table_t;
 
-// 해시 기반 세션 테이블
-extern session_data_t* session_table[HASH_TABLE_SIZE];
-
-// 함수 선언
-void init_session_table();
-unsigned int hash_session_key(session_key_t* key);
-session_data_t* find_or_create_session(struct ip* ip_hdr, struct tcphdr* tcp_hdr);
-void update_session(session_data_t* session, struct tcphdr* tcp_hdr);
-void dup_detection(session_data_t* session, struct tcphdr* tcp_hdr);
-void print_all_sessions();
-void free_session_table();
+unsigned int hash_key(const char *key);
+session_t *session_table_lookup(session_table_t *table, const char *key);
+session_t *session_table_insert(session_table_t *table, const char *key, const struct pcap_pkthdr *pkthdr);
 
 #endif
